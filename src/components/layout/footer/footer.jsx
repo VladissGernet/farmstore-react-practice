618956bbd50e7d9b2800ba3c6818092ae49b25d6
VladissGernet <skeimorr@gmail.com>
2025-02-26 19:41:17 +0400

    добавил дату в footer

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import { Container } from '/src/components/layout/container/container';
 import { Logo } from '/src/components/ui/logo/logo';
 
-import { StyledFooter } from './styled';
+import { StyledFooter, StyledDate } from './styled';
 
 const Footer = () => {
   return (
     <StyledFooter>
       <Container>
         <Logo />
+        <StyledDate>Создано 2021</StyledDate>
       </Container>
     </StyledFooter>
   )
--- src/components/layout/footer/styled.js
+++ src/components/layout/footer/styled.js
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
 
+import { StyledContainer } from '/src/components/layout/container/styled';
+
 const StyledFooter = styled.footer`
   background-color: ${(props) => props.theme.colorWhite};
   padding: 20px;
+  border-top: 1px solid ${(props) => props.theme.colorGray};
+
+  ${StyledContainer} {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
-export { StyledFooter };
+const StyledDate = styled.span`
+  color: ${(props) => props.theme.colorBlackForText};
+`;
+
+export { StyledFooter, StyledDate };
